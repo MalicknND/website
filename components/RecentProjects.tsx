9166a30a933efca110095026153abcd7034d47e3
MalicknND <ndiayemalick062@gmail.com>
2024-07-28 00:46:31 +0200

    ✅ chore: Update RecentProjects component and data index

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -4,6 +4,7 @@ import { FaLocationArrow } from "react-icons/fa6";
 
 import { projects } from "@/data";
 import { PinContainer } from "./ui/Pin";
+import Image from "next/image";
 
 const RecentProjects = () => {
   return (
@@ -24,12 +25,13 @@ const RecentProjects = () => {
                   className="relative w-full h-full overflow-hidden lg:rounded-3xl"
                   style={{ backgroundColor: "#13162D" }}
                 >
-                  <img src="/bg.png" alt="bgimg" />
+                  <Image src="/bg.png" alt="bgimg" fill={true} />
                 </div>
-                <img
+                <Image
                   src={item.img}
                   alt="cover"
                   className="z-10 absolute bottom-0"
+                  fill={true}
                 />
               </div>
 
@@ -57,7 +59,12 @@ const RecentProjects = () => {
                         transform: `translateX(-${5 * index + 2}px)`,
                       }}
                     >
-                      <img src={icon} alt="icon" className="p-2" />
+                      <Image
+                        src={icon}
+                        alt="icon"
+                        className="p-2"
+                        fill={true}
+                      />
                     </div>
                   ))}
                 </div>
